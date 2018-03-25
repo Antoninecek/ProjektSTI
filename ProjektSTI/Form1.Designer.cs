d65198bd2360d9a7f8a8597c8993e2a4f35a40c7
Radek_Krejci <radek.krejci@tul.cz>
2018-03-25 18:19:33 +0200

    Úprava UI a času

--- ProjektSTI/Form1.Designer.cs
+++ ProjektSTI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ProjektSTI
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Vyžaduje se proměnná návrháře.
@@ -28,57 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.RefreshButton = new System.Windows.Forms.Button();
+            this.LogBox = new System.Windows.Forms.RichTextBox();
+            this.TimeShower = new System.Windows.Forms.Label();
+            this.ClearLogBoxButton = new System.Windows.Forms.Button();
+            this.ChangedFilesBox = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.AllFilesTreeView = new System.Windows.Forms.TreeView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // RefreshButton
+            // 
+            this.RefreshButton.Enabled = false;
+            this.RefreshButton.Location = new System.Drawing.Point(12, 12);
+            this.RefreshButton.Name = "RefreshButton";
+            this.RefreshButton.Size = new System.Drawing.Size(52, 39);
+            this.RefreshButton.TabIndex = 0;
+            this.RefreshButton.Text = "Refresh";
+            this.RefreshButton.UseVisualStyleBackColor = true;
+            this.RefreshButton.Click += new System.EventHandler(this.RefreshButton_Click);
+            // 
+            // LogBox
+            // 
+            this.LogBox.Location = new System.Drawing.Point(12, 57);
+            this.LogBox.Name = "LogBox";
+            this.LogBox.ReadOnly = true;
+            this.LogBox.Size = new System.Drawing.Size(206, 386);
+            this.LogBox.TabIndex = 3;
+            this.LogBox.Text = "";
+            // 
+            // TimeShower
+            // 
+            this.TimeShower.AutoSize = true;
+            this.TimeShower.Location = new System.Drawing.Point(70, 25);
+            this.TimeShower.Name = "TimeShower";
+            this.TimeShower.Size = new System.Drawing.Size(148, 13);
+            this.TimeShower.TabIndex = 4;
+            this.TimeShower.Text = "Další kontrola za: 0h 00m 00s";
+            this.TimeShower.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ClearLogBoxButton
+            // 
+            this.ClearLogBoxButton.Enabled = false;
+            this.ClearLogBoxButton.Location = new System.Drawing.Point(73, 450);
+            this.ClearLogBoxButton.Name = "ClearLogBoxButton";
+            this.ClearLogBoxButton.Size = new System.Drawing.Size(75, 23);
+            this.ClearLogBoxButton.TabIndex = 5;
+            this.ClearLogBoxButton.Text = "Clear";
+            this.ClearLogBoxButton.UseVisualStyleBackColor = true;
+            this.ClearLogBoxButton.Click += new System.EventHandler(this.ClearLogBoxButton_Click);
+            // 
+            // ChangedFilesBox
+            // 
+            this.ChangedFilesBox.Location = new System.Drawing.Point(452, 57);
+            this.ChangedFilesBox.Name = "ChangedFilesBox";
+            this.ChangedFilesBox.ReadOnly = true;
+            this.ChangedFilesBox.Size = new System.Drawing.Size(222, 386);
+            this.ChangedFilesBox.TabIndex = 6;
+            this.ChangedFilesBox.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Red;
+            this.pictureBox1.Location = new System.Drawing.Point(648, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(25, 26);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(135, 128);
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(295, 449);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(68, 82);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Graf";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // richTextBox1
+            // AllFilesTreeView
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(251, 23);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(336, 577);
-            this.richTextBox1.TabIndex = 2;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.AllFilesTreeView.Location = new System.Drawing.Point(224, 57);
+            this.AllFilesTreeView.Name = "AllFilesTreeView";
+            this.AllFilesTreeView.PathSeparator = "/";
+            this.AllFilesTreeView.Size = new System.Drawing.Size(222, 386);
+            this.AllFilesTreeView.TabIndex = 9;
             // 
-            // richTextBox2
-            // 
-            this.richTextBox2.Location = new System.Drawing.Point(371, 13);
-            this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(400, 576);
-            this.richTextBox2.TabIndex = 3;
-            this.richTextBox2.Text = "";
-            // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(783, 601);
-            this.Controls.Add(this.richTextBox2);
-            this.Controls.Add(this.richTextBox1);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(688, 487);
+            this.Controls.Add(this.AllFilesTreeView);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ChangedFilesBox);
+            this.Controls.Add(this.ClearLogBoxButton);
+            this.Controls.Add(this.TimeShower);
+            this.Controls.Add(this.LogBox);
+            this.Controls.Add(this.RefreshButton);
+            this.Name = "MainForm";
+            this.Text = "Projekt STI";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
+        private System.Windows.Forms.Button RefreshButton;
+        private System.Windows.Forms.RichTextBox LogBox;
+        private System.Windows.Forms.Label TimeShower;
+        private System.Windows.Forms.Button ClearLogBoxButton;
+        private System.Windows.Forms.RichTextBox ChangedFilesBox;
+        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.TreeView AllFilesTreeView;
     }
 }
 
--- ProjektSTI/Form1.cs
+++ ProjektSTI/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,59 +15,167 @@ using System.Windows.Forms;
 
 namespace ProjektSTI
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
         static System.Windows.Forms.Timer casovac = new System.Windows.Forms.Timer();
-        static Cas cas = new Cas(10000);
+        static int interval = 3600000 - 1000; // -1 sekunda kvůli správné časové synchronizaci
+        static Cas cas = new Cas(interval);
         static Stopwatch sw = new Stopwatch();
 
-        public Form1()
+        static Boolean noveSpusteni = true;
+
+        public MainForm()
         {
             InitializeComponent();
             casovac.Tick += new EventHandler(ZpracovaniCasovace);
-            //casovac.Interval = 3600000;
-            //casovac.Interval = 1000;
-            //casovac.Start();
+            casovac.Interval = 1000;
+            casovac.Start();
+
+            LogBox.SelectAll();
+            LogBox.SelectionAlignment = HorizontalAlignment.Center;
+
             sw.Restart();
         }
 
         private static async void ZpracovaniCasovace(Object objekt, EventArgs eventargs)
         {
-            Program.form1.richTextBox1.AppendText((Math.Round((Decimal)sw.ElapsedMilliseconds/1000)).ToString() + " sekund \n");
             cas.OdectiSekunduAktualnihoCasu();
-            Program.form1.richTextBox1.AppendText(cas.VratAktualniCasFormat() + "\n");
-            if (cas.VratAktualniCasMs() == 0)
+            Program.MainForm.TimeShower.Text = "Další kontrola za: " + cas.VratAktualniCasFormat();
+            ArrayList vsechnySoubory = new ArrayList();
+            ArrayList zmeneneSoubory = new ArrayList();
+            NastavKontrolkuInternetu();
+
+            if (noveSpusteni == true)
             {
+                noveSpusteni = false;
                 Sluzba s = new Sluzba();
                 
-                Program.form1.richTextBox2.AppendText("Zpracovavam commity" + "\n");
+                Program.MainForm.LogBox.AppendText("---------------- " + DateTime.Now.ToString("hh:mm:ss tt") + " ----------------" + "\n");
+                Program.MainForm.LogBox.AppendText("Zpracovavam commity..." + "\n");
                 var commity = await s.VratSouboryCommituPoCaseAsync(DateTime.Now.AddYears(-5));
-                Program.form1.richTextBox2.AppendText("Pocet commitu: " + commity.Count + "\n");
+                Program.MainForm.LogBox.AppendText("Pocet commitu: " + commity.Count + "\n");
+
+                foreach (File commit in commity)
+                {
+                    if (!vsechnySoubory.Contains(commit.filename.ToString()))
+                    {
+                        vsechnySoubory.Add(commit.filename.ToString());
+                    }
+                }
+                vsechnySoubory.Sort();
+                VypisSeznamVsechSouboru(vsechnySoubory);
+
                 var jazyky = await s.SpocitejPocetRadkuVSouborechUrcitehoTypuAsync("java");
-                Program.form1.richTextBox2.AppendText("Pocet radku jazyku Java: " + jazyky.ToString() + "\n");
+                Program.MainForm.LogBox.AppendText("Pocet radku jazyku Java: " + jazyky.ToString() + "\n\n");
+                AktivovatTlacitka(true);
+                Program.MainForm.button1.Enabled = true;
+            }
+            if (cas.VratAktualniCasMs() == 0)
+            {
+                AktivovatTlacitka(false);
+                Sluzba s = new Sluzba();
+                Program.MainForm.LogBox.AppendText("---------------- " + DateTime.Now.ToString("hh:mm:ss tt") + " ----------------" + "\n");
+                Program.MainForm.LogBox.AppendText("Zpracovavam commity..." + "\n");
+                var commity = await s.VratSouboryCommituPoCaseAsync(DateTime.Now.AddMilliseconds(-1*(interval-(int)cas.VratAktualniCasMs())));
+                //var commity = await s.VratSouboryCommituPoCaseAsync(DateTime.Now.AddDays(-7)); // Pro testovací účely
+                Program.MainForm.LogBox.AppendText("Pocet commitu: " + commity.Count + "\n");
+                var jazyky = await s.SpocitejPocetRadkuVSouborechUrcitehoTypuAsync("java");
+                Program.MainForm.LogBox.AppendText("Pocet radku jazyku Java: " + jazyky.ToString() + "\n\n");
+                if (commity.Count > 0){
+                    foreach (File commit in commity)
+                    {
+                        if (!zmeneneSoubory.Contains(commit.filename.ToString()))
+                        {
+                            zmeneneSoubory.Add(commit.filename.ToString());
+                        }
+                    }
+                    VypisSeznamZmenenychSouboru(zmeneneSoubory);
+                    zmeneneSoubory.Clear();
+                }
+                
+
+                AktivovatTlacitka(true);
             }
             
+
         }
 
-        private async void button1_Click(object sender, EventArgs e)
+        private static void NastavKontrolkuInternetu()
         {
-            Sluzba s = new Sluzba();
-            var souboryCommitu = await s.VratSouboryCommituPoCaseAsync(DateTime.Now.AddHours(-1));
-            var jazyky = await s.SpocitejPocetRadkuVSouborechUrcitehoTypuAsync("java");
-            var js = await s.VratStatistikuZmenyRadkuSouboruAsync("JAVASOUBOR3.java");
-            var radky = RootObject.SpocitejPocetRadkuSadySouboru(RootObject.VratSouboryUrcitehoTypuRepozitare("java"));
-
+            if (ZkouskaInternetovehoPripojeni())
+            {
+                Program.MainForm.pictureBox1.BackColor = Color.Green;
+            } else
+            {
+                Program.MainForm.pictureBox1.BackColor = Color.Red;
+            }
         }
 
-
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        private static void AktivovatTlacitka(Boolean b)
         {
-
+            Program.MainForm.RefreshButton.Enabled = b;
+            Program.MainForm.ClearLogBoxButton.Enabled = b;
         }
 
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        private static void VypisSeznamVsechSouboru(ArrayList soubory)
         {
+            //Program.MainForm.AllFilesBox.Clear();
+            
+            foreach (String soubor in soubory)
+            {
+                //Program.MainForm.AllFilesBox.AppendText(soubor + "\n");
+                Program.MainForm.AllFilesTreeView.Nodes.Add(soubor);
+            }
+        }
+
+        private static void VypisSeznamZmenenychSouboru(ArrayList soubory)
+        {
+            foreach (String soubor in soubory)
+            {
+                Program.MainForm.ChangedFilesBox.AppendText(soubor + "\n");
+            }
+        }
+
+        private static bool ZkouskaInternetovehoPripojeni()
+        {
+            try
+            {
+                using (var client = new System.Net.WebClient())
+                {
+                    using (client.OpenRead("http://clients3.google.com/generate_204"))
+                    {
+                        Console.WriteLine("Internet pripojen");
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Internet nepripojen");
+                return false;
+            }
+        }
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            AktivovatTlacitka(false);
+            while (cas.VratAktualniCasMs() != 1000)
+            {
+                cas.OdectiSekunduAktualnihoCasu();
+            }
 
         }
+
+        private void ClearLogBoxButton_Click(object sender, EventArgs e)
+        {
+            Program.MainForm.LogBox.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 GraphForm = new Form2(Program.MainForm.AllFilesTreeView.SelectedNode.Text);
+            GraphForm.Show();
+        }
+
     }
 }
