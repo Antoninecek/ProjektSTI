8758947da2fe88f072c95370ac4ea7576fc1db79
Antoninecek <frantisek.jukl@gmail.com>
2018-03-26 21:26:19 +0200

    pridani attr datum_commitu pro File
    pridani attr datum_commitu pro File, init loggeru, pokus o disable cache pri requestu

---
+++ ProjektSTI/Logger.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace ProjektSTI
+{
+    class Logger
+    {
+
+        public void OdesliEmail()
+        {
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new NetworkCredential("frantisek.jukl@gmail.com", "FR4N7!53KJUKL");
+            client.Port = 587;
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true;
+
+            try
+            {
+                MailAddress
+                    maFrom = new MailAddress("sender_email@domain.tld", "Sender's Name", Encoding.UTF8),
+                    maTo = new MailAddress("frantisek.jukl@gmail.com", "Recipient's Name", Encoding.UTF8);
+                MailMessage mmsg = new MailMessage(maFrom.Address, maTo.Address);
+                mmsg.Body = "<html><body><h1>Some HTML Text for Test as BODY</h1></body></html>";
+                mmsg.BodyEncoding = Encoding.UTF8;
+                mmsg.IsBodyHtml = true;
+                mmsg.Subject = "Some Other Text as Subject";
+                mmsg.SubjectEncoding = Encoding.UTF8;
+
+                client.Send(mmsg);
+                
+            }
+            catch (Exception ex)
+            {
+                
+                //throw;
+            }
+        }
+
+    }
+}
