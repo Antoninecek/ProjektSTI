a4f8a350c569eabe06ff06257486781d0c9c835b
Antoninecek <frantisek.jukl@gmail.com>
2018-03-29 00:12:50 +0200

    mail

--- ProjektSTI/Logger.cs
+++ ProjektSTI/Logger.cs
@@ -13,28 +13,6 @@ namespace ProjektSTI
 
         public void OdesliEmail()
         {
-<<<<<<< HEAD
-            SmtpClient client = new SmtpClient();
-            client.Credentials = new NetworkCredential("frantisek.jukl@gmail.com", "pass");
-            client.Port = 587;
-            client.Host = "smtp.gmail.com";
-            client.EnableSsl = true;
-
-            try
-            {
-                MailAddress
-                    maFrom = new MailAddress("sender_email@domain.tld", "Sender's Name", Encoding.UTF8),
-                    maTo = new MailAddress("frantisek.jukl@gmail.com", "Recipient's Name", Encoding.UTF8);
-                MailMessage mmsg = new MailMessage(maFrom.Address, maTo.Address);
-                mmsg.Body = "<html><body><h1>Some HTML Text for Test as BODY</h1></body></html>";
-                mmsg.BodyEncoding = Encoding.UTF8;
-                mmsg.IsBodyHtml = true;
-                mmsg.Subject = "Some Other Text as Subject";
-                mmsg.SubjectEncoding = Encoding.UTF8;
-
-                client.Send(mmsg);
-                
-=======
             try
             {
                 MailMessage mail = new MailMessage();
@@ -50,11 +28,10 @@ namespace ProjektSTI
                 SmtpServer.EnableSsl = true;
 
                 SmtpServer.Send(mail);
->>>>>>> 2844fcd11fe8d5e10b21e26431200142bff7e84f
             }
             catch (Exception ex)
             {
-                
+
                 //throw;
             }
         }
